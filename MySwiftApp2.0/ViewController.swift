aaff546fecdea38644d5a2bbcf2cd59c44d83086
Nila Dharmaraj <nila@dharmaraj.com>
2019-07-17 11:27:01 -0700

    Initial Commit

---
+++ MySwiftApp2.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MySwiftApp2.0
+//
+//  Created by 24NilaDharmaraj on 7/17/19.
+//  Copyright © 2019 Nila Dharmaraj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
